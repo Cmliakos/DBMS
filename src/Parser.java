c15c63002c7c40951566c15f885f7c29077e4461
Chris Liakos <cmliakos@Chriss-MacBook-Pro.local>
2026-03-26 23:32:14 -0400

    Start parser by identifying the keywords that can be at the very beginning of a line

--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,39 @@
+import java.util.List;
+
 public class Parser {
-    //init
+    private final List<Token> tokens;
+    private int current;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+        this.current = 0;
+    }
+
+    public Command parseStatement() {
+        if (matchKeyword("CREATE")) {
+            return parseCreate();
+        } if (matchKeyword("USE")) {
+            return parseUse();
+        } if (matchKeyword("DESCRIBE")) {
+            return parseDescribe();
+        } if (matchKeyword("INSERT")) {
+            return parseInsert();
+        } if (matchKeyword("UPDATE")) {
+            return parseUpdate();
+        } if (matchKeyword("SELECT")) {
+            return parseSelect();
+        } if (matchKeyword("DELETE")) {
+            return parseDelete();
+        } if (matchKeyword("RENAME")) {
+            return parseRename();
+        } if (matchKeyword("LET")) {
+            return parseLet();
+        } if (matchKeyword("INPUT")) {
+            return parseInput();
+        } if (matchKeyword("EXIT")) {
+            return parseExit();
+        }
+
+        throw new RuntimeException("Unexpected token: " + peek());
+    }
 }
